ee19106e24e439c831747c5e56c1fa472f222cd0
saprykins <60004824+saprykins@users.noreply.github.com>
2022-09-05 17:15:18 +0200

    Update infa_login.py

--- scripts/infa_login.py
+++ scripts/infa_login.py
@@ -1,85 +1,7 @@
+# no idea what to change more
+
 import requests
 import os
-
-'''
-'''
-
-# connect to INFA
-
-# MY PART
-from datetime import timedelta
-import json
-
-def open_session():
-    # LOGIN TO INFA
-
-    # is used to get icSessionId
-    url = 'https://dm-em.informaticacloud.com/ma/api/v2/user/login'
-
-    myobj = {
-        "@type":"login",
-        "username":"TestServiceAccount",
-        "password":"***"
-    }
-
-    # x is response from INFA
-    x = requests.post(url, json = myobj)
-    # make response as json to be able to read as dictionary
-    json_obj = x.json()
-
-    # informatica session id
-    session_id = json_obj["icSessionId"]
-    # print(session_id)
-    
-    # session_id below is INFA session id
-    # ti.xcom_push(key='session_id', value=session_id)
-    
-    return session_id
-
-
-def run_a_task(session_id):
-    # START A TASK
-
-    url_job = 'https://emw1.dm-em.informaticacloud.com/saas/api/v2/job'
-    
-    # get session_id from another function
-    
-    Headers = {"icSessionId": session_id}
-
-    myobj = {
-        "@type":"job",
-        "taskId":"0119EH0I000000000002",
-        "taskType":"DSS"
-    }
-
-    y = requests.post(url_job, headers=Headers, json = myobj)
-    print(y)
-
-
-
-def close_session(session_id):
-    # CLOSE SESSION
-
-    url_logout = "https://dm-em.informaticacloud.com/ma/api/v2/user/logout"
-
-    myobj = {
-        "@type":"logout"
-    }
-    z = requests.post(url_logout, json = myobj)
-    return "session is closed"
-
-session_id = open_session()
-run_a_task(session_id)
-close_session(session_id)
-
-
-
-
-
-
-# ORIGINAL PART
-
-
 
 
 URL = os.environ['IICS_LOGIN_URL']
@@ -89,7 +11,10 @@ PASSWORD = os.environ['IICS_PASSWORD']
 UAT_USERNAME = os.environ['UAT_IICS_USERNAME']
 UAT_PASSWORD = os.environ['UAT_IICS_PASSWORD']
 
-URL = "https://dm-us.informaticacloud.com/saas/public/core/v3/login"
+# ORIGINAL
+# URL = "https://dm-em.informaticacloud.com/saas/public/core/v3/login"
+URL = "https://dm-em.informaticacloud.com/ma/api/v2/user/login"
+
 BODY = {"username": USERNAME,"password": PASSWORD}
 
 r = requests.post(url = URL, json = BODY)
