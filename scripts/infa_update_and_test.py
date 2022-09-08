ed4fd8f75edb0aae9c3bbd633ff9c88880d3bd99
saprykins <60004824+saprykins@users.noreply.github.com>
2022-09-08 13:34:27 +0200

    Update infa_update_and_test.py

--- scripts/infa_update_and_test.py
+++ scripts/infa_update_and_test.py
@@ -1,5 +1,5 @@
 # 
-
+"""
 import requests
 import os
 import json
@@ -83,3 +83,4 @@ for x in r_filtered:
         print("Mapping task: " + activity_log[0]['objectName'] + " completed successfully. ")
 
 requests.post(URL + "/public/core/v3/logout", headers = HEADERS)
+"""
